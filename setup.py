ba5000573e705bc5c4f2a986ffa0b830e97e7c07
Gabriel S Chaves <33940064+gschaves@users.noreply.github.com>
2024-01-23 14:45:35 -0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+VERSION_FILE = "version.txt"
+INIT_FILE = "torch_neuronx/__init__.py"
+
+def get_version():
+    with open(VERSION_FILE, encoding="utf-8") as f:
+        ver = f.readline()
+
+    if ver.startswith("for"):
+        with open(INIT_FILE, encoding="utf-8") as f:
+            for line in f.readlines():
+                if line.startswith("__version__"):
+                    ver = line.split()[-1].strip('"') + "+master"
+
+    return ver
+
+setup(
+    name='torch_neuronx',
+    version=get_version(),
+    packages=find_packages()
+)
